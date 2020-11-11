30c7893c7e125ade6ae9fe7a9240d84bb82a8043
Delver Villanueva <xdelver_live@outlook.com>
2020-11-11 09:58:20 -0500

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,27 @@
 // PUERTO
 // ========================================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ========================================
+// ENTORNO
+// ========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ========================================
+// BASE DE DATOS
+// ========================================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://devervilla:oB7GKaZqri4yc7yQ@cluster0.nzvyo.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,42 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
  
-app.get('/usuario', function (req, res) { //
-  res.json('get usuario');
-});
 
+app.use( require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) { //
+mongoose.connect(process.env.URLDB,
+                { useNewUrlParser: true, useCreateIndex: true},
+                (err, res)=>{
+    if(err) throw err;
 
-    let body = req.body;
-
-    if(body.nombre == undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
-    else{
-        res.json({
-            persona : body
-        });
-    }
-
-  });
-
-
-app.put('/usuario/:id', function (req, res) {//
-
-    let id = req.params.id;
-
-    res.json({
-        id 
-    });
-});
-
-
-app.delete('/usuario', function (req, res) {//
-    res.json('delete usuario');
+    console.log('Base de datos online');
 });
  
  
